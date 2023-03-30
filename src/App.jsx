5b6ee101091b9666c2c055caa74e766bd565f757
Ishankpathak <pathakishank4@gmail.com>
2023-03-30 15:45:04 +0530

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-
+import { BrowserRouter , Routes , Route  } from "react-router-dom";
 import { fetchDataFromApi } from "./utils/api";
 
 import { useSelector, useDispatch } from 'react-redux'
@@ -22,20 +22,34 @@ function App() {
 
 
   useEffect(()=>{
-   apiTesting();
+   fetchApiConfig();
   },[]);
 
-  const apiTesting = ()=>{
-    fetchDataFromApi("/movie/popular").then((res)=>{
+  const fetchApiConfig = ()=>{
+    fetchDataFromApi("/configuration").then((res)=>{
       console.log(res);
-      dispatch(getApiConfiguration(res));
+      const url = {
+        backdrop: res.images.secure_base_url+"original",
+        poster: res.images.secure_base_url+"original",
+        profile: res.images.secure_base_url+"original",
+      }
+      dispatch(getApiConfiguration(url));
     });
   };
   
-  return <div className="App">
-  App
-  {url?.total_pages}
-  </div>;
+  return (
+    <BrowserRouter>
+     <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/:mediaType/:id" element={<Details/>}/>
+          <Route path="/search/:query" element={<SearchResult/>}/>
+          <Route path="/explore/:mediaType" element={<Explore/>}/>
+          <Route path="*" element={<PageNotFound/>}/>
+        </Routes>
+      <Footer/>
+    </BrowserRouter>
+  )
 }
 
 export default App;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
-import './style.scss'
-const Header = () => {
-  return (
-    <div>
-      Header ishank
-    </div>
-  )
-}
+import React, { useState, useEffect } from "react";
+import { HiOutlineSearch } from "react-icons/hi";
+import { SlMenu } from "react-icons/sl";
+import { VscChromeClose } from "react-icons/vsc";
+import { useNavigate, useLocation } from "react-router-dom";
 
-export default Header
+import "./style.scss";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import logo from "../../assets/movix-logo.svg";
+
+const Header = () => {
+    const [show, setShow] = useState("top");
+    const [lastScrollY, setLastScrollY] = useState(0);
+    const [mobileMenu, setMobileMenu] = useState(false);
+    const [query, setQuery] = useState("");
+    const [showSearch, setShowSearch] = useState("");
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const openSearch = ()=>{
+      setMobileMenu(false)
+      setShowSearch(true)
+    }
+
+    const openMobileMenu = ()=>{
+      setMobileMenu(true)
+      setShowSearch(false)
+    }
+    return (
+        <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
+          <ContentWrapper>
+            <div className="logo">
+              <img src={logo} alt="" />
+            </div>
+            <div className="menuItems">
+              <li className="menuItem">Movies</li>
+              <li className="menuItem">TV shows</li>
+              <li className="menuItem">
+               <HiOutlineSearch/> 
+              </li>
+            </div>
+            <div className="mobileMenuItems">
+              <HiOutlineSearch/>
+              {
+                mobileMenu? <VscChromeClose onClick={()=>setMobileMenu(false)}/> : <SlMenu onClick={openMobileMenu}/>
+              }
+            </div>
+          </ContentWrapper>
+        </header>
+    );
+};
+
+export default Header;
