66215312327dfd89251d844611c61f20199883c7
Ishankpathak <pathakishank4@gmail.com>
2023-04-01 11:25:13 +0530

    trending section added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import HeroBanner from './heroBanner/HeroBanner'
 import './style.scss'
+import Trending from './trending/Trending'
+import Popular from './popular/Popular'
 
 const Home = () => {
   return (
     <div className='homePage'>
       <HeroBanner/>
+      <Trending/>
+      <Popular/>
+      <div style={{height:1000}}></div>
     </div>
   )
 }
