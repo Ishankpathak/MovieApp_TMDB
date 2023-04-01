8a4e41a60a9772bed73e81e3c59f6f95e307e3b6
Ishankpathak <pathakishank4@gmail.com>
2023-04-01 18:03:02 +0530

    Added video part

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import './style.scss'
+
+import useFetch from "../../hooks/useFetch"
+import DetailsBanner from './detailsBanner/DetailsBanner'
 const Details = () => {
+  const {mediaType , id} = useParams();
+  const {data , loading} = useFetch(`/${mediaType}/${id}/videos`)
+  const {data: credits , loading:creditsLoading} = useFetch(`/${mediaType}/${id}/credits`)
+
   return (
     <div>
-      Details
+    <DetailsBanner video={data?.result?.[0]} crew={credits?.crew}/>
+     
     </div>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,14 +3,14 @@ import HeroBanner from './heroBanner/HeroBanner'
 import './style.scss'
 import Trending from './trending/Trending'
 import Popular from './popular/Popular'
-
+import TopRated from './topRated/TopRated'
 const Home = () => {
   return (
     <div className='homePage'>
       <HeroBanner/>
       <Trending/>
       <Popular/>
-      <div style={{height:1000}}></div>
+      <TopRated/>
     </div>
   )
 }
